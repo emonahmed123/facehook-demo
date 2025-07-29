345cc3c6135b51492960cc7a2371d7eff2d109f6
Emon Ahmed <imonshomon@gmail.com>
2025-07-29 12:51:21 +0600

    create profile Reducers

---
+++ src/reducers/ProfileReducer.js
@@ -0,0 +1,15 @@
+// Write Profile Reducers
+
+
+const initialSate = {
+    user: null,
+    posts: [],
+    loading: false,
+    error: null
+}
+
+const profileReducer = () => {
+
+}
+
+export { initialSate, profileReducer }
