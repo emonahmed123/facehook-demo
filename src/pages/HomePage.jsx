aeacb28c1dd943176e7db9851925804e81d867f0
Emon Ahmed <imonshomon@gmail.com>
2025-08-04 10:07:48 +0600

    done Home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,50 @@
+import { useEffect, useReducer } from "react";
+import { actions } from "../actions";
+import PostList from "../components/posts/PostList";
+
+import { initialState, postReducer } from "../reducers/PostReducer";
+
+import useAxios from "../hooks/useAxios";
+
 const HomePage = () => {
+  const [state, dispatch] = useReducer(postReducer, initialState);
+  const { api } = useAxios();
+
+  useEffect(() => {
+    dispatch({ type: actions?.post?.DATA_FETCHING });
+
+    const fetchPost = async () => {
+      try {
+        const response = await api.get(`http://localhost:3000/posts`);
+        if (response.status === 200) {
+          dispatch({
+            type: actions?.post?.DATA_FETCHED,
+            data: response.data,
+          });
+        }
+      } catch (error) {
+        console.error(error);
+        dispatch({
+          type: actions.post.DATA_FETCH_ERROR,
+          error: error.message,
+        });
+      }
+    };
+
+    fetchPost();
+  }, []);
+
+  if (state?.loading) {
+    return <div> We are working...</div>;
+  }
+
+  if (state?.error) {
+    return <div> Error in fatching posts {state?.error?.message}</div>;
+  }
+
   return (
-    <div className="">
-      <h1>home page</h1>
+    <div className="container mx-auto px-4">
+      <PostList posts={state?.posts} />
     </div>
   );
 };
